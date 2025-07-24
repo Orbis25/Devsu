9d0570ca3fd7a70d8a0f0bef31c1d74de64cab4a
orbis25 <orbisalonzo@gmail.com>
2025-07-23 23:29:38 -0400

    fix: remove hardcoded license key from PDF service

--- backend/src/Devsu.Application/Services/Core/Pdf/PdfService.cs
+++ backend/src/Devsu.Application/Services/Core/Pdf/PdfService.cs
@@ -11,11 +11,11 @@ public class PdfService : IPdfService
     public byte[] GeneratePdfAsBytes(string? html = "")
     {
         Installation.LinuxAndDockerDependenciesAutoConfig = true;
-        License.LicenseKey = "IRONSUITE.ORBIS.ALONZO.UNICARIBE.EDU.DO.8326-C5C3E2FB5B-G6BW27T56NMHDS-VBLXRNHM4QF5-2T323ZHMLHIQ-UN4IVXVSIAEZ-KE2UEVSSIBIF-OEYDE2J3LDQV-RA6PKB-TGYJBEY6MDGQEA-DEPLOYMENT.TRIAL-LBWCQX.TRIAL.EXPIRES.22.AUG.2025";
+        License.LicenseKey = "";
         var renderer = new ChromePdfRenderer();
         var pdf = renderer.RenderHtmlAsPdf(html);
         using var ms = new MemoryStream();
         pdf.Stream.CopyTo(ms);
         return ms.ToArray();
     }
-}
+}   
